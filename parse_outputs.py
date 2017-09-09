f490fc92e976c2ce1c73cc9419f890a2887a1f05
darthbhyrava <hbhyrava@gmail.com>
2017-09-10 02:52:13 +0530

    Ready to start multi-indexing.

--- parse_outputs.py
+++ parse_outputs.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from nltk.stem.porter import PorterStemmer
 from stop_words import get_stop_words
 import nltk
@@ -24,186 +25,197 @@ def merge_d(d1, d2):
             union[key] = d1[key] + "+"+ d2[key]
     return union
 
-
+startTime = datetime.now()
 stop_words = get_stop_words('en')
 p_stemmer = PorterStemmer()
-file_num = 1
-count = 0
-all_dicts = []
+start = 1760
+end = 1764
 
-input_file = "./output/output_%d" % file_num
-output_file = "./indices/index_%d" % file_num
-fr = open('%s' % input_file, 'r+')
-fw = open('%s' % output_file, 'a+')
-content = fr.read().split('~~~~')
 
-for text in content:
-    #getting title
-    title = find_between(text, "<title>", "</title>")
-    text = text.replace(title, '')
-    #getting infobox
-    info = find_between(text, "{{Infobox", "}}")
-    text = text.replace(info, '')
-    #getting categories
-    cats = re.findall(r'\[\[Category:([^]]*)\]\]', text)
-    for i in cats:
-        text = text.replace(i, '')
-    #getting external links
-    extl = find_between(text, "xternal links==", "\n\n")
-    text = text.replace(extl, '')
-    #getting references
-    ref = find_between(text, "eferences==", "==") + find_between(text, "eferences ==", "==")
-    text = text.replace(ref, '')
+c = open('count_list', 'r+')
+lines = c.readlines()
+c.close()
 
-    #clearing up the dictionary, and working on each field
-    article_dict = {}
+for i in range(1760,1764):
+    all_dicts = []
+    pair = lines[i].split(':')
+    file_num = int(pair[0])+1
+    count = int(pair[1].split(',')[0].strip().replace('[','')) + 1
+    i_count = count
+    input_file = "./output/output_%d" % file_num
+    output_file = "./indices/index_%d" % file_num
+    fr = open('%s' % input_file, 'r+')
+    fw = open('%s' % output_file, 'a+')
+    content = fr.read().split('~~~~')
 
-    #TITLE
-    field_tokens = []
-    title = re.sub('[^A-Za-z]', ' ', title)
-    chunk = nltk.word_tokenize(title.lower())
-    stopped_tokens = [i for i in chunk if not i in stop_words]
-    for i in stopped_tokens:
-        try:
-            field_tokens.append(p_stemmer.stem(i))
-        except:
-            field_tokens.append(i)
-    for i in field_tokens:
-        if i in article_dict.keys():
-            freq = int(find_between(article_dict[i], "(",")")) + 1
-            if "T" in article_dict[i]:
-                article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+    for text in content:
+        #getting title
+        title = find_between(text, "<title>", "</title>")
+        text = text.replace(title, '')
+        #getting infobox
+        info = find_between(text, "{{Infobox", "}}")
+        text = text.replace(info, '')
+        #getting categories
+        cats = re.findall(r'\[\[Category:([^]]*)\]\]', text)
+        for i in cats:
+            text = text.replace(i, '')
+        #getting external links
+        extl = find_between(text, "xternal links==", "\n\n")
+        text = text.replace(extl, '')
+        #getting references
+        ref = find_between(text, "eferences==", "==") + find_between(text, "eferences ==", "==")
+        text = text.replace(ref, '')
+
+        #clearing up the dictionary, and working on each field
+        article_dict = {}
+
+        #TITLE
+        field_tokens = []
+        title = re.sub('[^A-Za-z]', ' ', title)
+        chunk = nltk.word_tokenize(title.lower())
+        stopped_tokens = [i for i in chunk if not i in stop_words]
+        for i in stopped_tokens:
+            try:
+                field_tokens.append(p_stemmer.stem(i))
+            except:
+                field_tokens.append(i)
+        for i in field_tokens:
+            if i in article_dict.keys():
+                freq = int(find_between(article_dict[i], "(",")")) + 1
+                if "T" in article_dict[i]:
+                    article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+                else:
+                    article_dict[i] = "T" + find_between(article_dict[i], "", "(") + "(%d)" % freq
             else:
-                article_dict[i] = "T" + find_between(article_dict[i], "", "(") + "(%d)" % freq
-        else:
-            article_dict[i] = "T%d(1)" % count
+                article_dict[i] = "T%d(1)" % count
 
-    #BODY TEXT
-    field_tokens = []
-    text = re.sub('[^A-Za-z]', ' ', text)
-    chunk = nltk.word_tokenize(text.lower())
-    stopped_tokens = [i for i in chunk if not i in stop_words]
-    for i in stopped_tokens:
-        try:
-            field_tokens.append(p_stemmer.stem(i))
-        except:
-            field_tokens.append(i)
-    for i in field_tokens:
-        if i in article_dict.keys():
-            freq = int(find_between(article_dict[i], "(",")")) + 1
-            if "B" in article_dict[i]:
-                article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+        #BODY TEXT
+        field_tokens = []
+        text = re.sub('[^A-Za-z]', ' ', text)
+        chunk = nltk.word_tokenize(text.lower())
+        stopped_tokens = [i for i in chunk if not i in stop_words]
+        for i in stopped_tokens:
+            try:
+                field_tokens.append(p_stemmer.stem(i))
+            except:
+                field_tokens.append(i)
+        for i in field_tokens:
+            if i in article_dict.keys():
+                freq = int(find_between(article_dict[i], "(",")")) + 1
+                if "B" in article_dict[i]:
+                    article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+                else:
+                    article_dict[i] = "B" + find_between(article_dict[i], "", "(") + "(%d)" % freq
             else:
-                article_dict[i] = "B" + find_between(article_dict[i], "", "(") + "(%d)" % freq
-        else:
-            article_dict[i] = "B%d(1)" % count
+                article_dict[i] = "B%d(1)" % count
 
-    # INFOBOX
-    field_tokens = []
-    info = re.sub('[^A-Za-z]', ' ', info)
-    chunk = info.lower()
-    chunk = nltk.word_tokenize(chunk)
-    stopped_tokens = [i for i in chunk if not i in stop_words]
-    for i in stopped_tokens:
-        try:
-            field_tokens.append(p_stemmer.stem(i))
-        except:
-            field_tokens.append(i)
-    for i in field_tokens:
-        if i in article_dict.keys():
-            freq = int(find_between(article_dict[i], "(",")")) + 1
-            if "I" in article_dict[i]:
-                article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+        # INFOBOX
+        field_tokens = []
+        info = re.sub('[^A-Za-z]', ' ', info)
+        chunk = info.lower()
+        chunk = nltk.word_tokenize(chunk)
+        stopped_tokens = [i for i in chunk if not i in stop_words]
+        for i in stopped_tokens:
+            try:
+                field_tokens.append(p_stemmer.stem(i))
+            except:
+                field_tokens.append(i)
+        for i in field_tokens:
+            if i in article_dict.keys():
+                freq = int(find_between(article_dict[i], "(",")")) + 1
+                if "I" in article_dict[i]:
+                    article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+                else:
+                    article_dict[i] = "I" + find_between(article_dict[i], "", "(") + "(%d)" % freq
             else:
-                article_dict[i] = "I" + find_between(article_dict[i], "", "(") + "(%d)" % freq
-        else:
-            article_dict[i] = "+I%d(1)" % count
+                article_dict[i] = "+I%d(1)" % count
 
-    # CATEGORIES
-    field_tokens = []
-    cat = " ".join(cats)
-    cat = re.sub('[^A-Za-z]', ' ', cat)
-    chunk = cat.lower()
-    chunk = nltk.word_tokenize(chunk)
-    stopped_tokens = [i for i in chunk if not i in stop_words]
-    for i in stopped_tokens:
-        try:
-            field_tokens.append(p_stemmer.stem(i))
-        except:
-            field_tokens.append(i)
-    for i in field_tokens:
-        if i in article_dict.keys():
-            freq = int(find_between(article_dict[i], "(",")")) + 1
-            if "C" in article_dict[i]:
-                article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+        # CATEGORIES
+        field_tokens = []
+        cat = " ".join(cats)
+        cat = re.sub('[^A-Za-z]', ' ', cat)
+        chunk = cat.lower()
+        chunk = nltk.word_tokenize(chunk)
+        stopped_tokens = [i for i in chunk if not i in stop_words]
+        for i in stopped_tokens:
+            try:
+                field_tokens.append(p_stemmer.stem(i))
+            except:
+                field_tokens.append(i)
+        for i in field_tokens:
+            if i in article_dict.keys():
+                freq = int(find_between(article_dict[i], "(",")")) + 1
+                if "C" in article_dict[i]:
+                    article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+                else:
+                    article_dict[i] = "C" + find_between(article_dict[i], "", "(") + "(%d)" % freq
             else:
-                article_dict[i] = "C" + find_between(article_dict[i], "", "(") + "(%d)" % freq
-        else:
-            article_dict[i] = "C%d(1)" % count
+                article_dict[i] = "C%d(1)" % count
 
-    # REFERENCES
-    field_tokens = []
-    ref = re.sub('[^A-Za-z]', ' ', ref)
-    chunk = ref.lower()
-    chunk = nltk.word_tokenize(chunk)
-    stopped_tokens = [i for i in chunk if not i in stop_words]
-    for i in stopped_tokens:
-        try:
-            field_tokens.append(p_stemmer.stem(i))
-        except:
-            field_tokens.append(i)
-    for i in field_tokens:
-        if i in article_dict.keys():
-            freq = int(find_between(article_dict[i], "(",")")) + 1
-            if "R" in article_dict[i]:
-                article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+        # REFERENCES
+        field_tokens = []
+        ref = re.sub('[^A-Za-z]', ' ', ref)
+        chunk = ref.lower()
+        chunk = nltk.word_tokenize(chunk)
+        stopped_tokens = [i for i in chunk if not i in stop_words]
+        for i in stopped_tokens:
+            try:
+                field_tokens.append(p_stemmer.stem(i))
+            except:
+                field_tokens.append(i)
+        for i in field_tokens:
+            if i in article_dict.keys():
+                freq = int(find_between(article_dict[i], "(",")")) + 1
+                if "R" in article_dict[i]:
+                    article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+                else:
+                    article_dict[i] = "R" + find_between(article_dict[i], "", "(") + "(%d)" % freq
             else:
-                article_dict[i] = "R" + find_between(article_dict[i], "", "(") + "(%d)" % freq
-        else:
-            article_dict[i] = "R%d(1)" % count
+                article_dict[i] = "R%d(1)" % count
 
-    # EXTERNAL LINKS
-    field_tokens = []
-    extl = re.sub('[^A-Za-z]', ' ', extl)
-    chunk = extl.lower()
-    chunk = nltk.word_tokenize(chunk)
-    stopped_tokens = [i for i in chunk if not i in stop_words]
-    for i in stopped_tokens:
-        try:
-            field_tokens.append(p_stemmer.stem(i))
-        except:
-            field_tokens.append(i)
-    for i in field_tokens:
-        if i in article_dict.keys():
-            freq = int(find_between(article_dict[i], "(",")")) + 1
-            if "E" in article_dict[i]:
-                article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+        # EXTERNAL LINKS
+        field_tokens = []
+        extl = re.sub('[^A-Za-z]', ' ', extl)
+        chunk = extl.lower()
+        chunk = nltk.word_tokenize(chunk)
+        stopped_tokens = [i for i in chunk if not i in stop_words]
+        for i in stopped_tokens:
+            try:
+                field_tokens.append(p_stemmer.stem(i))
+            except:
+                field_tokens.append(i)
+        for i in field_tokens:
+            if i in article_dict.keys():
+                freq = int(find_between(article_dict[i], "(",")")) + 1
+                if "E" in article_dict[i]:
+                    article_dict[i] = find_between(article_dict[i], "", "(") + "(%d)" % freq
+                else:
+                    article_dict[i] = "E" + find_between(article_dict[i], "", "(") + "(%d)" % freq
             else:
-                article_dict[i] = "E" + find_between(article_dict[i], "", "(") + "(%d)" % freq
-        else:
-            article_dict[i] = "E%d(1)" % count
+                article_dict[i] = "E%d(1)" % count
 
-    count += 1
-    sys.stdout.write("\rIndexing: %d pages done." % (count))
-    sys.stdout.flush()
-    all_dicts.append(article_dict)
+        count += 1
+        sys.stdout.write("\rIndexing: %d pages done." % (count-i_count))
+        sys.stdout.flush()
+        all_dicts.append(article_dict)
 
 
-print "Merging..."
+    print "\nMerging..."
 
-while len(all_dicts) > 1:
-    copy_all = all_dicts[:]
-    all_dicts[:] = []
-    c_len = len(copy_all)/2
-    for ind in range(c_len):
-        all_dicts.append(merge_d(copy_all[ind], copy_all[ind+c_len]))
+    while len(all_dicts) > 1:
+        copy_all = all_dicts[:]
+        all_dicts[:] = []
+        c_len = len(copy_all)/2
+        for ind in range(c_len):
+            all_dicts.append(merge_d(copy_all[ind], copy_all[ind+c_len]))
 
-print  "Merging done. Now writing indices ..."
+    print  "Writing indices..."
 
-article_dict = sorted(all_dicts[0].items())
-for key, value in article_dict:
-    fw.write("%s:%s\n" % (key, value))
-fw.close()
-fr.close()
+    article_dict = sorted(all_dicts[0].items())
+    for key, value in article_dict:
+        fw.write("%s:%s\n" % (key, value))
+    fw.close()
+    fr.close()
 
-print "Indexing done.\n"
+    print datetime.now() - startTime
+    print "Indexing done till block numbers: %d. Progress: %d/300\n" % (file_num, (file_num-start))
